238efa213de0e993f27cff991476758bf8b44917
nonumeros <kaploceh@gmail.com>
2023-05-03 02:51:46 -0400

    updating

---
+++ go.mod
@@ -0,0 +1,5 @@
+module kakoune
+
+go 1.20
+
+require github.com/mawww/kakoune v2022.10.31+incompatible // indirect
---
+++ kakoune.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "github.com/mawww/kakoune"
+)
+
+func main() {
+    _ = kakoune.daeditor()
+}
